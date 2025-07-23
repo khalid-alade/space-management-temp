9718d5ce261afe70980cbd799b8cdea82d47c657
Yusuf Khalid <khalidalade2004@gmail.com>
2025-07-23 16:36:07 +0100

    second commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,27 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: {
+//     unoptimized: true,
+//   },
+// }
+
+// export default nextConfig
+
+
+
+
+
+
+
+
+import { withNetlify } from '@netlify/next';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -9,6 +33,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default withNetlify(nextConfig);
